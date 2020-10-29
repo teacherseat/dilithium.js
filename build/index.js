9ab743571fee27e91edeb893b2f53af4e217c05c
Andrew Brown <andrew@monsterboxpro.com>
2020-10-29 18:55:08 -0400

    Commit build directory

---
+++ build/index.js
@@ -0,0 +1,245 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "m", {
+  enumerable: true,
+  get: function get() {
+    return _mithril.m;
+  }
+});
+Object.defineProperty(exports, "redraw", {
+  enumerable: true,
+  get: function get() {
+    return _mithril.redraw;
+  }
+});
+Object.defineProperty(exports, "trust", {
+  enumerable: true,
+  get: function get() {
+    return _mithril.trust;
+  }
+});
+Object.defineProperty(exports, "mount", {
+  enumerable: true,
+  get: function get() {
+    return _mithril.mount;
+  }
+});
+Object.defineProperty(exports, "route", {
+  enumerable: true,
+  get: function get() {
+    return _mithril.route;
+  }
+});
+Object.defineProperty(exports, "stream", {
+  enumerable: true,
+  get: function get() {
+    return _stream["default"];
+  }
+});
+Object.defineProperty(exports, "$register", {
+  enumerable: true,
+  get: function get() {
+    return _events.$register;
+  }
+});
+Object.defineProperty(exports, "$broadcast", {
+  enumerable: true,
+  get: function get() {
+    return _events.$broadcast;
+  }
+});
+Object.defineProperty(exports, "ApiBase", {
+  enumerable: true,
+  get: function get() {
+    return _api.ApiBase;
+  }
+});
+Object.defineProperty(exports, "Component", {
+  enumerable: true,
+  get: function get() {
+    return _component.Component;
+  }
+});
+Object.defineProperty(exports, "View", {
+  enumerable: true,
+  get: function get() {
+    return _view.View;
+  }
+});
+Object.defineProperty(exports, "Util", {
+  enumerable: true,
+  get: function get() {
+    return _util.Util;
+  }
+});
+Object.defineProperty(exports, "Model", {
+  enumerable: true,
+  get: function get() {
+    return _model.Model;
+  }
+});
+Object.defineProperty(exports, "Attribute", {
+  enumerable: true,
+  get: function get() {
+    return _attribute.Attribute;
+  }
+});
+Object.defineProperty(exports, "AttributeArray", {
+  enumerable: true,
+  get: function get() {
+    return _attribute_array.AttributeArray;
+  }
+});
+Object.defineProperty(exports, "AttributeModelArray", {
+  enumerable: true,
+  get: function get() {
+    return _attribute_model_array.AttributeModelArray;
+  }
+});
+Object.defineProperty(exports, "$location", {
+  enumerable: true,
+  get: function get() {
+    return _location.$location;
+  }
+});
+Object.defineProperty(exports, "Popup", {
+  enumerable: true,
+  get: function get() {
+    return _popup.Popup;
+  }
+});
+Object.defineProperty(exports, "Form", {
+  enumerable: true,
+  get: function get() {
+    return _form.Form;
+  }
+});
+Object.defineProperty(exports, "Checkbox", {
+  enumerable: true,
+  get: function get() {
+    return _checkbox.Checkbox;
+  }
+});
+Object.defineProperty(exports, "ErrorMsg", {
+  enumerable: true,
+  get: function get() {
+    return _err.ErrorMsg;
+  }
+});
+Object.defineProperty(exports, "FileField", {
+  enumerable: true,
+  get: function get() {
+    return _file.FileField;
+  }
+});
+Object.defineProperty(exports, "InputPassword", {
+  enumerable: true,
+  get: function get() {
+    return _input_password.InputPassword;
+  }
+});
+Object.defineProperty(exports, "InputText", {
+  enumerable: true,
+  get: function get() {
+    return _input_text.InputText;
+  }
+});
+Object.defineProperty(exports, "Radio", {
+  enumerable: true,
+  get: function get() {
+    return _radio.Radio;
+  }
+});
+Object.defineProperty(exports, "Textarea", {
+  enumerable: true,
+  get: function get() {
+    return _textarea.Textarea;
+  }
+});
+Object.defineProperty(exports, "ListView", {
+  enumerable: true,
+  get: function get() {
+    return _list.ListView;
+  }
+});
+Object.defineProperty(exports, "ShowView", {
+  enumerable: true,
+  get: function get() {
+    return _show.ShowView;
+  }
+});
+Object.defineProperty(exports, "MemberView", {
+  enumerable: true,
+  get: function get() {
+    return _member.MemberView;
+  }
+});
+Object.defineProperty(exports, "CollectionView", {
+  enumerable: true,
+  get: function get() {
+    return _collection.CollectionView;
+  }
+});
+exports.Link = exports.param = void 0;
+
+var _mithril = require("mithril");
+
+var _stream = _interopRequireDefault(require("mithril/stream"));
+
+var _events = require("./operations/events/events");
+
+var _api = require("./operations/api/api");
+
+var _component = require("./operations/component/component");
+
+var _view = require("./operations/view/view");
+
+var _util = require("./operations/util/util");
+
+var _model = require("./operations/model/model");
+
+var _attribute = require("./operations/attribute/attribute");
+
+var _attribute_array = require("./operations/attribute_array/attribute_array");
+
+var _attribute_model_array = require("./operations/attribute_model_array/attribute_model_array");
+
+var _location = require("./operations/location/location");
+
+var _popup = require("./sciences/popup/popup");
+
+var _form = require("./sciences/form/form");
+
+var _checkbox = require("./sciences/fields/checkbox/checkbox");
+
+var _err = require("./sciences/fields/err/err");
+
+var _file = require("./sciences/fields/file/file");
+
+var _input_password = require("./sciences/fields/input_password/input_password");
+
+var _input_text = require("./sciences/fields/input_text/input_text");
+
+var _radio = require("./sciences/fields/radio/radio");
+
+var _textarea = require("./sciences/fields/textarea/textarea");
+
+var _list = require("./command/list/list");
+
+var _show = require("./command/show/show");
+
+var _member = require("./command/member/member");
+
+var _collection = require("./command/collection/collection");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// Generated by CoffeeScript 2.4.1
+var Link, param;
+exports.param = param;
+exports.Link = Link;
+exports.param = param = _mithril.route.param;
+exports.Link = Link = _mithril.route.Link;
